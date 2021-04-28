092106bc94c826d5ec70675f0251305774354945
Mr Sam Law <sam@chongland.com>
2021-04-28 23:52:34 +1000

    Updated README.md, remove redundant files

--- README.md
+++ README.md
@@ -1,23 +1,27 @@
-# 02-HW-ProfessionalPortfolio
-
-## Description
-
-The beginnings of my portfolio showcasing development work, with scope for more work to be added in.
-
-## What We Did
-The site was wireframed and designed with a mobile-first approach.
-We consolidated the CSS styling by combining duplicate/redundant styling declarations.
+# Mr Sam Law - Full Stack Developer Updated Porfolio
+![Starting Screen](assets/images/Portfolio.png)
 
 ## Deployment
 The portfolio has been deployed at:
-[Professional Portfolio GitHub](https://mrsamlaw.github.io/02-HW-ProfessionalPortfolio/)
+[https://mrsamlaw.github.io/08-ProfessionalMaterials/](https://mrsamlaw.github.io/08-ProfessionalMaterials/)
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Technologies
+- HTML
+- CSS
+- Bootstrap
+- JavaScript
+
+## Description
+An update of my portfolio showcasing development work, with scope for more work to be added in.
+
+## What We Did
+Some backend upgrades, using objects for Portfolio items, and styling most of the page utilising Bootstrap
 
 ## Screenshot
 ![The Professional Portfolio.](./assets/images/ProfessionalPortfolio.png)
 
-## Wireframes
-![Home Screen](./assets/images/01-Home.png)
-![Menu Screen](./assets/images/02-Menu.png)
-![Portfolio Screen](./assets/images/03-Portfolio.png)
-![About Screen](./assets/images/04-About.png)
-![Contact Us Screen](./assets/images/05-Contact.png)
+## Inspiration
+The color styling was inspired by this image: Ferrari 288 GTO by artist Michał Zagórski (https://dribbble.com/mvcnphorm).
+![Ferrari 288 GTO By Michał Zagórski.png](assets/images/Ferrari288GTOByMichalZagorski.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,28 @@
 const repos = {
     1: {
-        title: "",
-        description: "",
-        tech: ["HTML", "CSS", "JavaScript", "Bootstrap", "GoogleFonts"],
-        repo: "",
-        URL: "",
-        screenshot: "",
+        title: "ANZAC Bubble Travel App",
+        description: "A dynamic, user friendly, and convenient travel and accomodation finder for Australians that are keen to visit our New Zealand neighbours!",
+        tech: ["HTML", "CSS", "JavaScript", "Bootstrap", "JQuery", "Moment JS", "APIs"],
+        repo: "https://github.com/MrSamLaw/07-ANZAC-Bubble-Travel-App",
+        URL: "https://mrsamlaw.github.io/07-ANZAC-Bubble-Travel-App/",
+        screenshot: "./assets/images/ANZ-Travel-Bubble-Landing.png",
     },
     2: {
-        title: "Two",
-        description: "",
-        tech: "",
-        repo: "",
-        URL: "",
-        screenshot: "",
+        title: "Everyweather You Go! - Weather Dashboard",
+        description: "A weather dashboard that uses API calls to determine the weather of a city",
+        tech: ["HTML", "CSS", "JavaScript", "Bootstrap", "JQuery", "Moment JS", "API"],
+        repo: "https://github.com/MrSamLaw/06-WeatherDashboard",
+        URL: "https://mrsamlaw.github.io/06-WeatherDashboard/",
+        screenshot: "./assets/images/WeatherDashboard.png",
     },
 }
 
 let printRepos = (repo) => {
     for (const key in repo) {
-        console.log(`${key}\n Title: ${repo[key].title}`);
         let techString = repo[key].tech = repo[key].tech.join(" | ");
         var repoCard = document.createElement("div");
         repoCard.innerHTML = [
-            `<div class="col-md-12 col-lg-6 col-sm-3 my-3">
-            <div class="card card-primary text-center">
+            `<div class="card card-primary text-center p-2">
                 <img class="card-img-top"
                     src="${repo[key].screenshot}"
                     alt="Card image cap">
@@ -32,14 +30,14 @@ let printRepos = (repo) => {
                     <h4 class="card-title">${repo[key].title}</h4>
                     <p class="card-text">${techString}</p>
                     <p class="card-text">${repo[key].description}</p>
-                    <a href="${repo[key].repo}"
+                    <a href="${repo[key].repo}" target="_blank"
                         class="btn btn-primary">GitHub Repository</a> 
-                    <a href="${repo[key].URL}"
+                    <a href="${repo[key].URL}" target="_blank"
                         class="btn btn-primary">Deployed Site</a>
                 </div>
-            </div>
-        </div>`
+            </div>`
         ];
+        repoCard.classList.add("col-md-4", "col-lg-6", "col-sm", "my-3");
         document.querySelector("#portfolioCards").appendChild(repoCard);
     }
 }
